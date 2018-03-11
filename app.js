1801797d2d018b32fd574e840f4f456118bb3963
Enejo James <spec.en.james@gmail.com>
2018-03-11 19:41:45 +0100

    Testing Travis, Hound and Mocha setup

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+const greeting = () => 'Test is working';
+
 app.listen(1447, () => {
     console.log('Now listening on 1447');
 });
+
+export default greeting;
--- test/test.app.js
+++ test/test.app.js
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import greeting from '../app';
 
 describe('App', () => {
-    it('greeting should return "its working"', () => {
-        assert.equal(greeting(), 'its working');
+    it('greeting should return "Test is working"', () => {
+        assert.equal(greeting(), 'Test is working');
     });
 });
