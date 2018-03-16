77881321a014e5b815275e44f19a76df0461f07f
Enejo James <spec.en.james@gmail.com>
2018-03-17 00:23:18 +0100

    added routes

--- server/controllers/businesses.js
+++ server/controllers/businesses.js
@@ -6,6 +6,87 @@ const allBusinesses = (req, res) => res.status(200).json({
     businesses
 });
 
+// Get details of a business
+const businessDetails = (req, res) => {
+    let theBusiness;
+    businesses.forEach((business) => {
+        if (parseInt(req.params.businessId, 10) === business.id) {
+            theBusiness = business;
+        }
+    });
+    if (theBusiness) {
+        return res.status(200).json({
+            message: 'Business found',
+            business: theBusiness
+        });
+    }
+    return res.status(404).json({
+        message: 'Business not found'
+    });
+};
+
+// Display create business form on POST
+const createBusiness = (req, res) => {
+    const { company, category, location } = req.body;
+    const business = {
+        id: (businesses.length + 1),
+        company,
+        category,
+        location
+    };
+    businesses.push(business);
+    return res.status(201).json({
+        message: 'Business created',
+        business
+    });
+};
+
+// Handle delete business on DELETE
+const deleteBusiness = (req, res) => {
+    let theBusiness;
+    businesses.forEach((business, index) => {
+        if (parseInt(req.params.businessId, 10) === business.id) {
+            theBusiness = business;
+            businesses.splice(index, 1);
+        }
+    });
+    if (theBusiness) {
+        return res.status(200).json({
+            message: 'Business deleted',
+            business: theBusiness
+        });
+    }
+    return res.status(404).json({
+        message: 'Business not found'
+    });
+};
+
+// Handle update business on PUT
+const updateBusiness = (req, res) => {
+    const { company, category, location } = req.body;
+    const updateObject = {
+        company,
+        category,
+        location
+    };
+    let theBusiness;
+    businesses.forEach((business) => {
+        if (parseInt(req.params.businessId, 10) === business.id) {
+            business = { ...business, ...updateObject };
+            theBusiness = business;
+        }
+    });
+    if (theBusiness) {
+        return res.status(200).json({
+            message: 'Business updated',
+            business: theBusiness
+        });
+    }
+    return res.status(404).json({
+        message: 'Business not found'
+    });
+};
+
 // exporting
 export {
     allBusinesses,
