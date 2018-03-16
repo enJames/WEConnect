be522dde561a8376b761751ff0e34b31e8e454c9
Enejo James <spec.en.james@gmail.com>
2018-03-17 00:06:27 +0100

    added tests

--- test/test.app.js
+++ test/test.app.js
@@ -27,27 +27,6 @@ describe('Test for businesses', () => {
         });
     });
 
-    describe('when the user sends a POST request to /api/v1/businesses/', () => {
-        it('It should return a 201 status and create a new business', (done) => {
-            chai.request(app)
-                .post('/api/v1/businesses')
-                .send({
-                    company: 'Some Nigeria Limited',
-                    category: 'Human Resources',
-                    location: 'Lagos'
-                })
-                .end((err, res) => {
-                    res.should.have.status(201);
-                    res.body.should.be.a('object');
-                    assert.isString(
-                        res.body.message,
-                        'Business created'
-                    );
-                    done();
-                });
-        });
-    });
-
     describe('when the user sends a GET request to /api/v1/businesses/:businessId', () => {
         it('It should return a 200 status and retrive a business with id of 1', (done) => {
             chai.request(app)
@@ -77,61 +56,6 @@ describe('Test for businesses', () => {
                     assert.isString(
                         res.body.message,
                         'Business not found'
-                    );
-                    done();
-                });
-        });
-    });
-
-    describe('when the user sends a DELETE request to /api/v1/businesses/:businessId', () => {
-        it('It should return a 200 status and retrieve deleted business with id of 1', (done) => {
-            chai.request(app)
-                .delete('/api/v1/businesses/1')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.business.should.be.a('object');
-                    assert.equal(
-                        res.body.message,
-                        'Business deleted'
-                    );
-                    done();
-                });
-        });
-
-        it('It should return a 404 status when req.params is 40', (done) => {
-            chai.request(app)
-                .delete('/api/v1/businesses/40')
-                .end((err, res) => {
-                    res.should.have.status(404);
-                    res.body.should.be.a('object');
-                    assert.isUndefined(
-                        res.body.business,
-                        'business does not exist'
-                    );
-                    assert.isString(
-                        res.body.message,
-                        'Business not found'
-                    );
-                    done();
-                });
-        });
-    });
-
-    describe('when the user sends an UPDATE request to /api/v1/businesses/:businessId', () => {
-        it('It should return a 200 status and retrieve deleted business with id of 2', (done) => {
-            chai.request(app)
-                .put('/api/v1/businesses/2')
-                .send({
-                    company: 'Fife consult'
-                })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.business.should.be.a('object');
-                    assert.isString(
-                        res.body.message,
-                        'Business deleted'
                     );
                     done();
                 });
