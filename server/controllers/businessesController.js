7df073962e777c63f676755c5431d0ec1c2475ba
Enejo James <spec.en.james@gmail.com>
2018-03-17 22:05:40 +0100

    Modified /businesses: included search feature

--- server/controllers/businessesController.js
+++ server/controllers/businessesController.js
@@ -1,10 +1,32 @@
 import businesses from '../models/businessData';
 
 // Get list of all businesses
-const allBusinesses = (req, res) => res.status(200).json({
-    message: 'All businesses',
-    businesses
-});
+const allBusinesses = (req, res) => {
+    const searchLocation = req.query.location;
+    const result = [];
+
+    if (!searchLocation) {
+        return res.status(200).json({
+            message: 'All businesses',
+            businesses
+        });
+    }
+    businesses.forEach((business) => {
+        if (searchLocation === business.location) {
+            result.push(business);
+        }
+    });
+
+    if (result.length > 0) {
+        return res.status(200).json({
+            message: `Found ${result.lenth} businesses in ${searchLocation}`,
+            result
+        });
+    }
+    return res.status(404).json({
+        message: 'No registered business in this location'
+    });
+};
 
 // Get details of a business
 const businessDetails = (req, res) => {
@@ -43,16 +65,10 @@ const createBusiness = (req, res) => {
 
 // Handle update business on PUT
 const updateBusiness = (req, res) => {
-    const { company, category, location } = req.body;
-    const updateObject = {
-        company,
-        category,
-        location
-    };
     let theBusiness;
     businesses.forEach((business) => {
         if (parseInt(req.params.businessId, 10) === business.id) {
-            business = { ...business, ...updateObject };
+            business = { ...business, ...req.body };
             theBusiness = business;
         }
     });
