f39dc6d8c51ae219445cc6cd8b6ef17839aaa74a
Enejo James <spec.en.james@gmail.com>
2018-03-17 01:14:53 +0100

    added a get-business-details feature

--- server/controllers/businesses.js
+++ server/controllers/businesses.js
@@ -25,67 +25,6 @@ const businessDetails = (req, res) => {
     });
 };
 
-// Display create business form on POST
-const createBusiness = (req, res) => {
-    const { company, category, location } = req.body;
-    const business = {
-        id: (businesses.length + 1),
-        company,
-        category,
-        location
-    };
-    businesses.push(business);
-    return res.status(201).json({
-        message: 'Business created',
-        business
-    });
-};
-
-// Handle delete business on DELETE
-const deleteBusiness = (req, res) => {
-    let theBusiness;
-    businesses.forEach((business, index) => {
-        if (parseInt(req.params.businessId, 10) === business.id) {
-            theBusiness = business;
-            businesses.splice(index, 1);
-        }
-    });
-    if (theBusiness) {
-        return res.status(200).json({
-            message: 'Business deleted',
-            business: theBusiness
-        });
-    }
-    return res.status(404).json({
-        message: 'Business not found'
-    });
-};
-
-// Handle update business on PUT
-const updateBusiness = (req, res) => {
-    const { company, category, location } = req.body;
-    const updateObject = {
-        company,
-        category,
-        location
-    };
-    let theBusiness;
-    businesses.forEach((business) => {
-        if (parseInt(req.params.businessId, 10) === business.id) {
-            business = { ...business, ...updateObject };
-            theBusiness = business;
-        }
-    });
-    if (theBusiness) {
-        return res.status(200).json({
-            message: 'Business updated',
-            business: theBusiness
-        });
-    }
-    return res.status(404).json({
-        message: 'Business not found'
-    });
-};
 
 // exporting
 export {
