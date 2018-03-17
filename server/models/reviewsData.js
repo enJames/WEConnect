af52b6816c8559f4d52f2d215482a30896911d1b
Enejo James <spec.en.james@gmail.com>
2018-03-17 13:45:32 +0100

    Added dummy data to reviewsData.js

--- server/models/reviewsData.js
+++ server/models/reviewsData.js
@@ -2,13 +2,25 @@ const reviews = [
     {
         id: 1,
         reviewer: 'John Doe',
-        reviewed: 'Some Company Nigeria Limited',
+        businessId: 1,
         reviewNote: 'A business you difinitely want to do business with'
     },
     {
         id: 2,
         reviewer: 'Jane Doe',
-        reviewed: 'Some Company Nigeria Limited',
+        businessId: 2,
+        reviewText: 'A customer-centric business'
+    },
+    {
+        id: 3,
+        reviewer: 'Jane Doe',
+        businessId: 2,
+        reviewText: 'A one-customer business'
+    },
+    {
+        id: 4,
+        reviewer: 'Jane Doe',
+        businessId: 1,
         reviewText: 'A customer-centric business'
     }
 ];
