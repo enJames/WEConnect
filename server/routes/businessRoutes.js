a75d203bcda60b00ee59c1dee10066f937ad6145
Enejo James <spec.en.james@gmail.com>
2018-03-17 02:38:14 +0100

    Added all routes

--- server/routes/businessRoutes.js
+++ server/routes/businessRoutes.js
@@ -12,4 +12,17 @@ const router = express.Router();
 // GET allBusinesses
 router.get('/', allBusinesses);
 
+// GET details of a business
+router.get('/:businessId', businessDetails);
+
+// Create a business
+router.post('/', createBusiness);
+
+// PUT details of a business
+router.put('/:businessId', updateBusiness);
+
+// DELETE a business
+router.delete('/:businessId', deleteBusiness);
+
+
 export default router;
