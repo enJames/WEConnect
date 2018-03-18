7a4c67266a4058b6b081206d48446322996c9556
Enejo James <spec.en.james@gmail.com>
2018-03-18 15:59:23 +0100

    Added users route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/businessRoutes';
+import userRoute from './routes/userRoutes';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1/businesses', router);
+app.use('/api/v1/auth', userRoute);
 
 app.listen(8000);
 
