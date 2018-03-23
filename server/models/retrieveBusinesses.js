f36719b87a3198a4ca54fc734c5604d197a3d72e
Enejo James <spec.en.james@gmail.com>
2018-03-23 15:20:49 +0100

    Removed files using dummy data

--- server/models/retrieveBusinesses.js
+++
@@ -1,7 +0,0 @@
-import connectToDatabase from '../connectToDatabase';
-
-const RetrieveBusinesses = () => {
-    connectToDatabase.query('SELECT * FROM businesses').then(businessTableRows => businessTableRows[0][0]);
-};
-
-export default RetrieveBusinesses;
